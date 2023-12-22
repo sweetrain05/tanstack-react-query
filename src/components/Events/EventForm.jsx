1abfafadf3a36b250d1beda480d102b8ffb8af81
Danbi Choi <sweetrain05@hotmail.com>
2023-12-21 16:38:33 -0800

    useQuery practice

--- src/components/Events/EventForm.jsx
+++ src/components/Events/EventForm.jsx
@@ -1,85 +1,103 @@
 import { useState } from 'react';
 
 import ImagePicker from '../ImagePicker.jsx';
+import { useQuery } from '@tanstack/react-query';
+import { fetchSelectableImages } from '../../util/http.js';
+import ErrorBlock from '../UI/ErrorBlock.jsx';
 
 export default function EventForm({ inputData, onSubmit, children }) {
-  const [selectedImage, setSelectedImage] = useState(inputData?.image);
+    const [selectedImage, setSelectedImage] = useState(inputData?.image);
 
-  function handleSelectImage(image) {
-    setSelectedImage(image);
-  }
+    const { data, isPending, isError } = useQuery({
+        queryKey: ['event-images'],
+        queryFn: fetchSelectableImages,
+    });
 
-  function handleSubmit(event) {
-    event.preventDefault();
+    function handleSelectImage(image) {
+        setSelectedImage(image);
+    }
 
-    const formData = new FormData(event.target);
-    const data = Object.fromEntries(formData);
+    function handleSubmit(event) {
+        event.preventDefault();
 
-    onSubmit({ ...data, image: selectedImage });
-  }
+        const formData = new FormData(event.target);
+        const data = Object.fromEntries(formData);
 
-  return (
-    <form id="event-form" onSubmit={handleSubmit}>
-      <p className="control">
-        <label htmlFor="title">Title</label>
-        <input
-          type="text"
-          id="title"
-          name="title"
-          defaultValue={inputData?.title ?? ''}
-        />
-      </p>
+        onSubmit({ ...data, image: selectedImage });
+    }
 
-      <div className="control">
-        <ImagePicker
-          images={[]}
-          onSelect={handleSelectImage}
-          selectedImage={selectedImage}
-        />
-      </div>
+    return (
+        <form id='event-form' onSubmit={handleSubmit}>
+            <p className='control'>
+                <label htmlFor='title'>Title</label>
+                <input
+                    type='text'
+                    id='title'
+                    name='title'
+                    defaultValue={inputData?.title ?? ''}
+                />
+            </p>
+            {isPending && <p>Loading selectable images...</p>}
+            {isError && (
+                <ErrorBlock
+                    title='Failed to load selectable images'
+                    message='Please try again.'
+                />
+            )}
+            {data && (
+                <div className='control'>
+                    <ImagePicker
+                        images={data}
+                        onSelect={handleSelectImage}
+                        selectedImage={selectedImage}
+                    />
 
-      <p className="control">
-        <label htmlFor="description">Description</label>
-        <textarea
-          id="description"
-          name="description"
-          defaultValue={inputData?.description ?? ''}
-        />
-      </p>
+                    {isError && <p>Failed to load images.</p>}
+                </div>
+            )}
 
-      <div className="controls-row">
-        <p className="control">
-          <label htmlFor="date">Date</label>
-          <input
-            type="date"
-            id="date"
-            name="date"
-            defaultValue={inputData?.date ?? ''}
-          />
-        </p>
+            <p className='control'>
+                <label htmlFor='description'>Description</label>
+                <textarea
+                    id='description'
+                    name='description'
+                    defaultValue={inputData?.description ?? ''}
+                />
+            </p>
 
-        <p className="control">
-          <label htmlFor="time">Time</label>
-          <input
-            type="time"
-            id="time"
-            name="time"
-            defaultValue={inputData?.time ?? ''}
-          />
-        </p>
-      </div>
+            <div className='controls-row'>
+                <p className='control'>
+                    <label htmlFor='date'>Date</label>
+                    <input
+                        type='date'
+                        id='date'
+                        name='date'
+                        defaultValue={inputData?.date ?? ''}
+                    />
+                </p>
 
-      <p className="control">
-        <label htmlFor="location">Location</label>
-        <input
-          type="text"
-          id="location"
-          name="location"
-          defaultValue={inputData?.location ?? ''}
-        />
-      </p>
+                <p className='control'>
+                    <label htmlFor='time'>Time</label>
+                    <input
+                        type='time'
+                        id='time'
+                        name='time'
+                        defaultValue={inputData?.time ?? ''}
+                    />
+                </p>
+            </div>
 
-      <p className="form-actions">{children}</p>
-    </form>
-  );
+            <p className='control'>
+                <label htmlFor='location'>Location</label>
+                <input
+                    type='text'
+                    id='location'
+                    name='location'
+                    defaultValue={inputData?.location ?? ''}
+                />
+            </p>
+
+            <p className='form-actions'>{children}</p>
+        </form>
+    );
 }
